bfbe6776a08ea5c36ded503459195189cb2a280e
Marcus Sanatan <msanatan@gmail.com>
2017-10-15 22:50:35 -0400

    Connect redux store to App component state

--- reminder-pro/src/components/App.js
+++ reminder-pro/src/components/App.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addReminder } from '../actions';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+  }
+
+  handleChangeInput = (event) => {
+    this.setState({
+      text: event.target.value
+    });
+  }
+
+  addReminder = () => {
+    this.props.addReminder(this.state.text);
+  }
+
   render() {
+    console.log('this.props', this.props);
     return (
         <div className='App'>
             <div className='title'>
@@ -12,11 +33,13 @@ class App extends Component {
                 <input
                   className='form-control'
                   placeholder='I have to...'
+                  onChange={this.handleChangeInput}
                 />
               </div>
               <button
                 type='button'
                 className='btn btn-success'
+                onClick={this.addReminder}
               >
                 Add Reminder
               </button>
@@ -26,4 +49,10 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    reminders: state
+  };
+}
+
+export default connect(mapStateToProps, {addReminder})(App);
