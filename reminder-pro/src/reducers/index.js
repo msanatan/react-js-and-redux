fba1806df8c315aeb1a1fec5ee675fed503dd578
Marcus Sanatan <msanatan@gmail.com>
2017-10-16 18:16:45 -0400

    Add delete reducer

--- reminder-pro/src/reducers/index.js
+++ reminder-pro/src/reducers/index.js
@@ -1,17 +1,28 @@
-import { ADD_REMINDER } from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
 
 const reminder = (action) => {
   return {
     text: action.text,
     id: Math.random()
   };
-}
+};
+
+const removeById = (state = [], id) => {
+  const reminders = state.filter(reminder => {
+    return reminder.id !== id;
+  });
+  return reminders;
+};
 
 const reminders = (state = [], action) => {
   let reminders = null;
   switch(action.type) {
     case ADD_REMINDER:
       reminders = [...state, reminder(action)];
+      console.log('reminders as state', reminders);
+      return reminders;
+    case DELETE_REMINDER:
+      reminders = removeById(state, action.id);
       console.log('reminders as state', reminders);
       return reminders;
     default:
